2efd4c613c8d67e4194efbd6e625bcd0cd535a49
JBP07 <00001109987420sp@al.educacao.sp.gov.br>
2024-06-17 21:05:12 -0300

    adicionando js e css

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-    createCanvas(600,600);
+    createCanvas(600, 600);
     background("black");
   }
 
-function draw() {
+  function draw() {
     stroke("blue");
-    Fill("red");
+    fill("red");
 
     if(mouseIspressed){
-        rect(mouseX, mouseY,20,35);
-    }
+     rect(mouseX, mouseY, 20, 35);
+   }
 
   }
